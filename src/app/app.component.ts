b45b4d51678556d3c47c922c2b122f73e93b8175
Jeffrey Meyer <jmeyer@swankmp.com>
2024-02-14 16:57:02 -0600

    Crude support to pick other theme colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeViewerComponent } from './theme-viewer/theme-viewer.component';
-import { MatFormField, MatInput } from '@angular/material/input';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, ThemeViewerComponent, MatInput, MatFormField],
+    imports: [RouterOutlet, ThemeViewerComponent, MatButton],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
@@ -14,10 +15,10 @@ export class AppComponent {
     title = 'theme-tinker';
     protected readonly document = document;
 
-    protected updateAccentColor($event: Event): void {
+    protected updateColor($event: Event, color: 'accent' | 'warn' | 'error' | 'success'): void {
         const target = $event.target;
         debugger;
         if (target instanceof HTMLInputElement)
-            document.documentElement.style.setProperty('--theme-accent', target.value);
+            document.documentElement.style.setProperty(`--theme-${color}`, target.value);
     }
 }
