b649001cf4d2f5ae4da9d77af969926b7e3b2775
Jeffrey Meyer <jmeyer@swankmp.com>
2024-02-14 15:39:22 -0600

    Crude support to pick theme accent colo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeViewerComponent } from './theme-viewer/theme-viewer.component';
+import { MatFormField, MatInput } from '@angular/material/input';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, ThemeViewerComponent],
+    imports: [RouterOutlet, ThemeViewerComponent, MatInput, MatFormField],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'theme-tinker';
+    protected readonly document = document;
+
+    protected updateAccentColor($event: Event): void {
+        const target = $event.target;
+        debugger;
+        if (target instanceof HTMLInputElement)
+            document.documentElement.style.setProperty('--theme-accent', target.value);
+    }
 }
